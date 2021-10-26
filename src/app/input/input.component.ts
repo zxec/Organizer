c15f09aa6cf39188c553de3baa8617e6bb48d2b5
Alexey Uzlov <auzlov@codemastersintl.com>
2021-10-26 12:05:19 +0300

    * added feedback

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+// имя компонента не совпадает с его целью, по сути это виджет task-form/task-edit, никак не app-input
+// app-input больше подходит компоненту формы, если требуется кастомный контрол формы
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
--- src/app/title/title.component.ts
+++ src/app/title/title.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+// этот компонент лишний, он не делает ничего полезного
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
